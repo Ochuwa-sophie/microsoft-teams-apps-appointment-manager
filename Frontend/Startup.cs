6d4b9d0a9737b95f394bb906ef3e2c6c8f13d854
Ochuwa-sophie <sophiecodes@gmail.com>
2021-04-13 12:00:00 +0100

    made changes to frontend

--- Frontend/Startup.cs
+++ Frontend/Startup.cs
@@ -13,6 +13,10 @@ namespace Frontend
 {
     public class Startup
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
